05f85489251b6db8139546d369f3875fb71e48a5
RandomAnimeGamer <iObjectGaming@gmail.com>
2017-02-04 13:07:49 -0700

    Fixed some movement and physics, will work on it later

---
+++ Rapporter/Rapporter/Rapporter.Game/RigidbodyMove.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SiliconStudio.Core.Mathematics;
+using SiliconStudio.Xenko.Input;
+using SiliconStudio.Xenko.Engine;
+using SiliconStudio.Xenko.Physics;
+
+namespace Rapporter { public class RigidbodyMove : SyncScript {
+    RigidbodyComponent shizuku;
+    private const float MaximumPitch = MathUtil.PiOverTwo * 0.99f;
+    private Vector3 upVector; private Vector3 translation;
+    private float yaw; private float pitch;
+
+    public Vector3 KeyboardMovementSpeed { get; set; } = new Vector3(5.0f);
+    public float SpeedFactor { get; set; } = 5.0f;
+    public Vector2 KeyboardRotationSpeed { get; set; } = new Vector2(3.0f);
+    public override void Start() { base.Start(); upVector = Vector3.UnitY; shizuku = Entity.Get<RigidbodyComponent>(); }
+    public override void Update() { ProcessInput(); UpdateTransform(); }
+
+    private void ProcessInput() {
+        translation = Vector3.Zero; yaw = 0; pitch = 0;
+
+        // Move with keyboard
+        if (Input.IsKeyDown(Keys.W)) translation.Z = -KeyboardMovementSpeed.Z;
+        else if (Input.IsKeyDown(Keys.S) || Input.IsKeyDown(Keys.Down)) { translation.Z = KeyboardMovementSpeed.Z; }
+
+        if (Input.IsKeyDown(Keys.A) || Input.IsKeyDown(Keys.Left)) { translation.X = -KeyboardMovementSpeed.X; }
+        else if (Input.IsKeyDown(Keys.D) || Input.IsKeyDown(Keys.Right)) { translation.X = KeyboardMovementSpeed.X; }
+
+        if (Input.IsKeyDown(Keys.Q)) { translation.Y = -KeyboardMovementSpeed.Y; }
+        else if (Input.IsKeyDown(Keys.E)) { translation.Y = KeyboardMovementSpeed.Y; }
+
+
+        if (Input.IsKeyDown(Keys.LeftShift) || Input.IsKeyDown(Keys.RightShift)) translation *= SpeedFactor;// Alt speed
+
+        // Rotate with keyboard
+//        if (Input.IsKeyDown(Keys.NumPad2)) { pitch = KeyboardRotationSpeed.X; }
+//        else if (Input.IsKeyDown(Keys.NumPad8)) { pitch = -KeyboardRotationSpeed.X; }
+//
+//        if (Input.IsKeyDown(Keys.NumPad4)) { yaw = KeyboardRotationSpeed.Y; }
+//        else if (Input.IsKeyDown(Keys.NumPad6)) { yaw = -KeyboardRotationSpeed.Y; }
+    }
+
+    private void UpdateTransform() {
+        var elapsedTime = (float)Game.UpdateTime.Elapsed.TotalSeconds;
+
+        translation *= elapsedTime;
+        yaw *= elapsedTime;
+        pitch *= elapsedTime;
+
+        var rotation = Matrix.RotationQuaternion(Entity.Transform.Rotation);// Get the local coordinate system
+
+        // Enforce the global up-vector by adjusting the local x-axis
+        var right = Vector3.Cross(rotation.Forward, upVector);
+        var up = Vector3.Cross(right, rotation.Forward);
+
+        // Stabilize
+        right.Normalize();
+        up.Normalize();
+
+        rotation.Right = right;
+        rotation.Up = up;
+
+        // Adjust pitch. Prevent it from exceeding up and down facing. Stabilize edge cases.
+        var currentPitch = MathUtil.PiOverTwo - (float)Math.Acos(Vector3.Dot(rotation.Forward, upVector));
+        pitch = MathUtil.Clamp(currentPitch + pitch, -MaximumPitch, MaximumPitch) - currentPitch;
+
+        // Move in local coordinates
+        shizuku.LinearVelocity += Vector3.TransformCoordinate(translation, rotation);
+        // Yaw around global up-vector, pitch and roll in local space
+//        shizuku.ApplyTorqueImpulse(new Vector3(yaw, 0, pitch) + upVector);
+  //          Quaternion.RotationMatrix(rotation) *
+  //          Quaternion.RotationAxis(right, pitch) *
+//            Quaternion.RotationAxis(upVector, yaw);
+    }
+} }
--- Rapporter/Rapporter/Rapporter.Game/obj/Windows/Debug/DataSerializers.cs
+++ Rapporter/Rapporter/Rapporter.Game/obj/Windows/Debug/DataSerializers.cs
@@ -25,9 +25,8 @@ namespace SiliconStudio.DataSerializers
     // Assembly attributes that defines supported serializer (only generics one so that other assemblies can do generic instantiations by themselves)
     [DataSerializerGlobalAttribute(typeof(SiliconStudio.DataSerializers.Rapporter_BasicCameraControllerSerializer), typeof(Rapporter.BasicCameraController), DataSerializerGenericMode.None, true, true, Profile = "Default")]
     [DataSerializerGlobalAttribute(typeof(SiliconStudio.DataSerializers.Rapporter_DebugPhysicsShapesSerializer), typeof(Rapporter.DebugPhysicsShapes), DataSerializerGenericMode.None, true, true, Profile = "Default")]
-    [DataSerializerGlobalAttribute(typeof(SiliconStudio.DataSerializers.Rapporter_GravitySerializer), typeof(Rapporter.Gravity), DataSerializerGenericMode.None, true, true, Profile = "Default")]
-    [DataSerializerGlobalAttribute(typeof(SiliconStudio.DataSerializers.Rapporter_JumpSerializer), typeof(Rapporter.Jump), DataSerializerGenericMode.None, true, true, Profile = "Default")]
     [DataSerializerGlobalAttribute(typeof(SiliconStudio.DataSerializers.Rapporter_MovementSerializer), typeof(Rapporter.Movement), DataSerializerGenericMode.None, true, true, Profile = "Default")]
+    [DataSerializerGlobalAttribute(typeof(SiliconStudio.DataSerializers.Rapporter_RigidbodyMoveSerializer), typeof(Rapporter.RigidbodyMove), DataSerializerGenericMode.None, true, true, Profile = "Default")]
    	public static class Rapporter_GameSerializerFactory
 	{
 	    [SiliconStudio.Core.ModuleInitializer]
@@ -52,9 +51,8 @@ namespace SiliconStudio.DataSerializers
 
 				assemblySerializersProfile.Add(new AssemblySerializerEntry(new SiliconStudio.Core.Storage.ObjectId(0xb387d334, 0x081a82a5, 0xd7a79b1f, 0x04b56377), typeof(Rapporter.BasicCameraController), typeof(SiliconStudio.DataSerializers.Rapporter_BasicCameraControllerSerializer)));
 				assemblySerializersProfile.Add(new AssemblySerializerEntry(new SiliconStudio.Core.Storage.ObjectId(0x2d184418, 0xc6fd7c15, 0x86fea326, 0xedd88efe), typeof(Rapporter.DebugPhysicsShapes), typeof(SiliconStudio.DataSerializers.Rapporter_DebugPhysicsShapesSerializer)));
-				assemblySerializersProfile.Add(new AssemblySerializerEntry(new SiliconStudio.Core.Storage.ObjectId(0x5c375119, 0x3502f6ca, 0x376746d2, 0x91176a1c), typeof(Rapporter.Gravity), typeof(SiliconStudio.DataSerializers.Rapporter_GravitySerializer)));
-				assemblySerializersProfile.Add(new AssemblySerializerEntry(new SiliconStudio.Core.Storage.ObjectId(0xc6ee40d8, 0x89605c78, 0xe57fa83d, 0x9b65b997), typeof(Rapporter.Jump), typeof(SiliconStudio.DataSerializers.Rapporter_JumpSerializer)));
 				assemblySerializersProfile.Add(new AssemblySerializerEntry(new SiliconStudio.Core.Storage.ObjectId(0xa652915e, 0x75e06b6e, 0x14d58d6f, 0xec39476d), typeof(Rapporter.Movement), typeof(SiliconStudio.DataSerializers.Rapporter_MovementSerializer)));
+				assemblySerializersProfile.Add(new AssemblySerializerEntry(new SiliconStudio.Core.Storage.ObjectId(0x051a538e, 0x04aeb45c, 0x7ed64eeb, 0xe5702132), typeof(Rapporter.RigidbodyMove), typeof(SiliconStudio.DataSerializers.Rapporter_RigidbodyMoveSerializer)));
 			}
 			{
 				var assemblySerializersProfile = new AssemblySerializersPerProfile();
@@ -175,37 +173,7 @@ namespace SiliconStudio.DataSerializers
 
 namespace SiliconStudio.DataSerializers
 {
-	sealed class Rapporter_GravitySerializer : ClassDataSerializer<Rapporter.Gravity>
-	{
-		private DataSerializer<SiliconStudio.Xenko.Engine.AsyncScript> parentSerializer;
-
-		public override void Initialize(SerializerSelector serializerSelector)
-		{
-			// Get parent serializer
-			parentSerializer = serializerSelector.GetSerializer<SiliconStudio.Xenko.Engine.AsyncScript>();
-			if (parentSerializer == null)
-				throw new InvalidOperationException(string.Format("Could not find parent serializer for type {0}", @"SiliconStudio.Xenko.Engine.AsyncScript"));
-			// Cache member serializers
-		}
-
-		public override void Serialize(ref Rapporter.Gravity obj, ArchiveMode mode, SerializationStream stream)
-		{
-			// Serialize parent (for now we don't copy reference back because it shouldn't change)
-			SiliconStudio.Xenko.Engine.AsyncScript parentObj = obj;
-			parentSerializer.Serialize(ref parentObj, mode, stream);
-			obj = (Rapporter.Gravity)parentObj;
-
-		}
-
-		internal static void ForceGenericInstantiation()
-		{
-		}
-	}
-}
-
-namespace SiliconStudio.DataSerializers
-{
-	sealed class Rapporter_JumpSerializer : ClassDataSerializer<Rapporter.Jump>
+	sealed class Rapporter_MovementSerializer : ClassDataSerializer<Rapporter.Movement>
 	{
 		private DataSerializer<SiliconStudio.Xenko.Engine.SyncScript> parentSerializer;
 
@@ -218,12 +186,12 @@ namespace SiliconStudio.DataSerializers
 			// Cache member serializers
 		}
 
-		public override void Serialize(ref Rapporter.Jump obj, ArchiveMode mode, SerializationStream stream)
+		public override void Serialize(ref Rapporter.Movement obj, ArchiveMode mode, SerializationStream stream)
 		{
 			// Serialize parent (for now we don't copy reference back because it shouldn't change)
 			SiliconStudio.Xenko.Engine.SyncScript parentObj = obj;
 			parentSerializer.Serialize(ref parentObj, mode, stream);
-			obj = (Rapporter.Jump)parentObj;
+			obj = (Rapporter.Movement)parentObj;
 
 		}
 
@@ -235,13 +203,12 @@ namespace SiliconStudio.DataSerializers
 
 namespace SiliconStudio.DataSerializers
 {
-	sealed class Rapporter_MovementSerializer : ClassDataSerializer<Rapporter.Movement>
+	sealed class Rapporter_RigidbodyMoveSerializer : ClassDataSerializer<Rapporter.RigidbodyMove>
 	{
 		private DataSerializer<SiliconStudio.Xenko.Engine.SyncScript> parentSerializer;
 		private DataSerializer<SiliconStudio.Core.Mathematics.Vector3> KeyboardMovementSpeedSerializer;
 		private DataSerializer<System.Single> SpeedFactorSerializer;
 		private DataSerializer<SiliconStudio.Core.Mathematics.Vector2> KeyboardRotationSpeedSerializer;
-		private DataSerializer<SiliconStudio.Core.Mathematics.Vector2> MouseRotationSpeedSerializer;
 
 		public override void Initialize(SerializerSelector serializerSelector)
 		{
@@ -255,12 +222,12 @@ namespace SiliconStudio.DataSerializers
 			KeyboardRotationSpeedSerializer = MemberSerializer<SiliconStudio.Core.Mathematics.Vector2>.Create(serializerSelector);
 		}
 
-		public override void Serialize(ref Rapporter.Movement obj, ArchiveMode mode, SerializationStream stream)
+		public override void Serialize(ref Rapporter.RigidbodyMove obj, ArchiveMode mode, SerializationStream stream)
 		{
 			// Serialize parent (for now we don't copy reference back because it shouldn't change)
 			SiliconStudio.Xenko.Engine.SyncScript parentObj = obj;
 			parentSerializer.Serialize(ref parentObj, mode, stream);
-			obj = (Rapporter.Movement)parentObj;
+			obj = (Rapporter.RigidbodyMove)parentObj;
 
             SiliconStudio.Core.Mathematics.Vector3 keyboardMovementSpeed = mode == ArchiveMode.Serialize ? obj.KeyboardMovementSpeed : default(SiliconStudio.Core.Mathematics.Vector3);
             KeyboardMovementSpeedSerializer.Serialize(ref keyboardMovementSpeed, mode, stream);
@@ -274,10 +241,6 @@ namespace SiliconStudio.DataSerializers
             KeyboardRotationSpeedSerializer.Serialize(ref keyboardRotationSpeed, mode, stream);
             if (mode == ArchiveMode.Deserialize)
                 obj.KeyboardRotationSpeed = keyboardRotationSpeed;
-            SiliconStudio.Core.Mathematics.Vector2 mouseRotationSpeed = mode == ArchiveMode.Serialize ? obj.MouseRotationSpeed : default(SiliconStudio.Core.Mathematics.Vector2);
-            KeyboardRotationSpeedSerializer.Serialize(ref mouseRotationSpeed, mode, stream);
-            if (mode == ArchiveMode.Deserialize)
-                obj.MouseRotationSpeed = mouseRotationSpeed;
 		}
 
 		internal static void ForceGenericInstantiation()
