8aea5fe4c8f47c257f1024c85ba029f22a55491b
RandomAnimeGamer <iObjectGaming@gmail.com>
2017-04-15 21:41:14 -0700

    Added dragon killing sword quest
    Xenko doesn't really want to open the project now for whatever reason.

---
+++ RapporterV2/RapporterV2/RapporterV2.Game/Quest/SwordQ.cs
@@ -0,0 +1,18 @@
+﻿﻿using System; using System.Collections.Generic; using System.Linq; using System.Text; using System.Threading.Tasks;
+using SiliconStudio.Core.Mathematics; using SiliconStudio.Xenko.Input;
+using SiliconStudio.Xenko.Engine; using SiliconStudio.Xenko.Engine.Events; using SiliconStudio.Xenko.Physics;
+
+namespace RapporterV2.Quest { public class SwordQ : AsyncScript {
+    public static readonly EventKey<bool> main = new EventKey<bool>();
+    public static readonly EventReceiver<bool> q = new EventReceiver<bool>(QuestManager.swordQ);
+    public override async Task Execute() {
+        var trigger = Entity.Get<PhysicsComponent>();
+        trigger.ProcessCollisions = true;
+        while(Game.IsRunning) {
+            var firstCollision = await trigger.NewCollision();//wait for entities coming in
+            var isQ = false; q.TryReceive(out isQ); if(isQ) main.Broadcast(true);
+            var collision = await trigger.CollisionEnded();
+            main.Broadcast(false);
+        }
+    }
+} }
