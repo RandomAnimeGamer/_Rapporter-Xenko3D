1cd0b3535f9c56c23226aec3d0abe72957380a40
RandomAnimeGamer <iObjectGaming@gmail.com>
2017-04-24 22:20:10 -0700

    Fixed combat, added NPC dialogue, added particles

--- FirstPersonShooter2/FirstPersonShooter2/FirstPersonShooter2.Game/Trigger/TriggerGroupException.cs
+++
@@ -1,9 +0,0 @@
-using System;
-
-namespace FirstPersonShooter2.Trigger
-{
-    public class TriggerGroupException : Exception
-    {
-        public TriggerGroupException(string ex) : base(ex) { }
-    }
-}
